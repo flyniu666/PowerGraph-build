bfcd17ef31a9994f72cff8d129284bdbd1081e68
flyniu666 <66696963@qq.com>
2022-04-08 06:56:14 -0400

    first commit

---
+++ build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+git clone https://github.com/flyniu666/PowerGraph-build.git
+git clone https://github.com/graphlab-code/graphlab.git
+
+rm -f graphlab/CMakeLists.txt
+cp PowerGraph-build/CMakeLists.txt graphlab
+rm -f graphlab/deps/tcmalloc/src/libtcmalloc/src/base/linuxthreads.cc
+cp PowerGraph-build/deps/tcmalloc/src/libtcmalloc/src/base/linuxthreads.cc  graphlab/deps/tcmalloc/src/libtcmalloc/src/base/
+
+mkdir graphlab/deps/hadoop/src/hadoop-stamp/hadoop-build -p
+mkdir -p graphlab/deps/hadoop/src/hadoop-stamp/hadoop-install
+
+if [ ! -f "graphlab/deps/hadoop/src/hadoop-stamp/hadoop-build/libhdfs.la" ];then
+  cp graphlab/deps/hadoop/src/hadoop/c++/Linux-amd64-64/lib/libhdfs.la   graphlab/deps/hadoop/src/hadoop-stamp/hadoop-build
+fi
+
+if [ ! -f "graphlab/deps/hadoop/src/hadoop-stamp/hadoop-install/libhdfs.la" ];then
+  cp graphlab/deps/hadoop/src/hadoop/c++/Linux-amd64-64/lib/libhdfs.la   graphlab/deps/hadoop/src/hadoop-stamp/hadoop-install
+fi
+
+if [ ! -f "/lib64/libhdfs.so" ];then
+  cp graphlab/deps/hadoop/src/hadoop/c++/Linux-amd64-64/lib/* /lib64
+fi
+
+cd graphlab/
+./configure
+cd release/
+make -j4
